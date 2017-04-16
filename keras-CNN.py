e482d3a04f089a979003fd396b0c2331f1f82eb7
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-04-17 03:17:31 +0800

    epochs=10

--- keras-CNN.py
+++ keras-CNN.py
@@ -66,7 +66,7 @@ model.compile(optimizer=adam,
 
 print('Training ------------')
 # Another way to train the model
-model.fit(X_train, y_train, epoch=1, batch_size=32,)
+model.fit(X_train, y_train, epochs=10, batch_size=32,)
 
 print('\nTesting ------------')
 # Evaluate the model with the metrics we defined earlier
