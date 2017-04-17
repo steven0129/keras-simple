95b5d60f25df5c1f25c9228d8578aad733c55e80
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-04-17 16:09:46 +0800

    加入keras-iris.py

---
+++ keras-iris.py
@@ -0,0 +1,53 @@
+import os
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+
+# download dataset
+response = urlopen(
+    'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
+data = response.read()
+fileName = './data/iris.csv'
+
+if(os.path.isfile(fileName)):
+    os.remove(fileName)
+
+file = open(fileName, 'wb')
+file.write(data)
+file.close()
+
+# load dataset
+dataframe = pandas.read_csv('./data/iris.csv', header=None)
+dataset = dataframe.values
+X = dataset[:, 0:4].astype(float)
+Y = dataset[:, 4]
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encodedY = encoder.transform(Y)
+dummyY = np_utils.to_categorical(encodedY)  # one-hot encoding
+
+# define baseline model
+def baseline_model():
+	# create model
+    # 4 inputs --> [4 hidden nodes] --> 3 outputs
+	model = Sequential()
+	model.add(Dense(4, input_dim=4, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(3, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
